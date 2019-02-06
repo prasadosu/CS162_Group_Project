352cdabbd011bd8de61289ee30002f19f657c80e
mouleja <mouleja@oregonstate.edu>
2019-02-05 19:54:49 -0800

    Updated class skeletons and Simulation

--- GroupProject/Critter.h
+++ GroupProject/Critter.h
@@ -2,18 +2,27 @@
 
 class Critter
 {
+public:
+	enum Type { Ant, Doodlebug };
+
 private:
 	bool moved;
+	int row;
+	int col;
+
 public:
-	enum Type{Ant, Doodlebug};
-	Critter() { moved = false; }
+	Critter(int row, int col)
+	{ 
+		this->row = row;
+		this->col = col;
+		this->moved = false;
+	}
 
-	bool hasMoved() { return moved; }
 	void setMoved(bool moved) { this->moved = moved; }
-
-	virtual void Move() = 0;
-	virtual void Breed() = 0;
+	bool hasMoved() { return moved; }
 	virtual Type GetType() = 0;
+	virtual void Move(Critter*** &board) = 0;
+	virtual void Breed(Critter*** &board) = 0;
 	virtual void Starve() { return; }
 	virtual ~Critter() {}
 };
