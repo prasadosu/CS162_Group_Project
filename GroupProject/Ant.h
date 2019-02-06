6e0d53524b5ffaf9d55c101a2caaa4f6e3e13ae4
DESKTOP-0Q9GVNG\Tim <drumboy354@sbcglobal.net>
2019-02-05 20:49:25 -0800

    Adding posX posY rows to ant

--- GroupProject/Ant.h
+++ GroupProject/Ant.h
@@ -4,9 +4,22 @@
 class Ant :
 	public Critter
 {
+private:
+	int posX;
+	int posY;
+
 public:
 	Ant(int row, int col);
 	~Ant();
+
+	// Getters
+	int getPosX() { return this->posX; }
+	int getPosY() { return this->posY; }
+
+	// Setters
+	void setPosX(int x) { this->posX = x; }
+	void setPosY(int y) { this->posY = y; }
+
 
 	void Move(Critter*** &board);
 	void Breed(Critter*** &board);
